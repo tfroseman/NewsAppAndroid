b8e7107bfd3934bff1e000606124eeb43936e960
Thomas Roseman <lindows.dev@gmail.com>
2014-11-28 21:20:53 -0500

    Added User class to store account info

--- app/src/main/java/com/newsappandroid/User.java
+++ app/src/main/java/com/newsappandroid/User.java
@@ -4,4 +4,35 @@ package com.newsappandroid;
  * Created by Thomas on 11/24/2014.
  */
 public class User {
+    protected String email;
+    protected String password;
+
+    public User(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
 }
