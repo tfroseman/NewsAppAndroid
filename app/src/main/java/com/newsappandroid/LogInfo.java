51fd7c8e7eb0820ebab56906e9bfa6a01f0fbceb
Thomas Roseman <lindows.dev@gmail.com>
2014-11-28 21:20:21 -0500

    Added LogInfo to help display to logcat cleanly

---
+++ app/src/main/java/com/newsappandroid/LogInfo.java
@@ -0,0 +1,34 @@
+package com.newsappandroid;
+
+import android.content.Intent;
+import android.util.Log;
+
+/**
+ * Created by Thomas on 11/28/2014.
+ */
+public class LogInfo {
+    public LogInfo() {
+    }
+
+    public void logUser(User user){
+        Log.i("User Info", user.toString());
+    }
+
+    public void logArticle(Article article){
+        Log.i("Article", article.toString());
+    }
+
+    public void logCategories(Categories categories){
+        Log.i("Categories", categories.toString());
+    }
+
+    public void logNetwork(NetworkConnection networkConnection){
+        Log.i("Network Connection", networkConnection.toString());
+    }
+
+    public void logIntent(Intent intent){
+        Log.i("Intent", intent.toString());
+    }
+
+
+}
