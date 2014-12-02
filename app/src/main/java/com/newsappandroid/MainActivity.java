b4ad647e6ac6744645f32d90313aa1b95b4614fd
Thomas Roseman <lindows.dev@gmail.com>
2014-12-02 16:32:33 -0500

    Cleaning up imports and  unused code

--- app/src/main/java/com/newsappandroid/MainActivity.java
+++ app/src/main/java/com/newsappandroid/MainActivity.java
@@ -1,58 +1,30 @@
 package com.newsappandroid;
 
 import android.app.Activity;
-import android.app.ActionBar;
-import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 
 public class MainActivity extends Activity {
-
-    private LogInfo logInfo = new LogInfo();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        if (savedInstanceState == null) {
-            getFragmentManager().beginTransaction()
-                    .add(R.id.container, new NewsFragment())
-                    .commit();
-        }
 
+        //Intent coming from login
         Intent intent = getIntent();
         Bundle extras = intent.getExtras();
 
-        //Parse to bundle from the intent
-        User user = new User(extras.getString("USER_EMAIL"), extras.getString("USER_PASSWORD"));
-        logInfo.logUser(user);
+        //Passing bundle from login to fragment view
+        NewsFragment newsFragment = new NewsFragment();
+        newsFragment.setArguments(extras);
+
+        setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getFragmentManager().beginTransaction()
+                    .add(R.id.container, newsFragment)
+                    .commit();
+        }
+
+
     }
-
-
-/*    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_news, menu);
-        return true;
-    }*/
-
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
 }
