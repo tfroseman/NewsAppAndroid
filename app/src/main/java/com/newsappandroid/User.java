4b6b1017210ce9f7ed8fec9de13eec9d882224c3
Thomas Roseman <lindows.dev@gmail.com>
2014-12-08 18:48:16 -0500

    Cleaned up class and added a few fields

--- app/src/main/java/com/newsappandroid/User.java
+++ app/src/main/java/com/newsappandroid/User.java
@@ -3,23 +3,23 @@ package com.newsappandroid;
 import java.util.Date;
 
 public class User {
-    private String email;
-    private String password;
 
     private int id;
-    private String first_name;
-    private Date updated_at;
-    private String api_token;
+    private String email, password;
+    private String first_name, last_name, api_token;
+    private String activated_at;
     private Boolean activated;
-    private String last_name;
-    private Date created_at;
-    private Boolean activated_at;
-    private Date last_login;
+    private Boolean signed_in;
+    private String last_login;
+    private String created_at;
+    private String updated_at;
 
+    private static User user = new User();
 
-    protected User(String email, String password) {
-        this.email = email;
-        this.password = password;
+    private User() {}
+
+    public static User getUser(){
+        return user;
     }
 
     public String getEmail() {
@@ -54,11 +54,11 @@ public class User {
         this.first_name = first_name;
     }
 
-    public Date getUpdated_at() {
+    public String getUpdated_at() {
         return updated_at;
     }
 
-    public void setUpdated_at(Date updated_at) {
+    public void setUpdated_at(String updated_at) {
         this.updated_at = updated_at;
     }
 
@@ -68,6 +68,10 @@ public class User {
 
     public void setApi_token(String api_token) {
         this.api_token = api_token;
+    }
+
+    public Boolean hasToken(){
+        return !this.getApi_token().isEmpty();
     }
 
     public Boolean getActivated() {
@@ -86,27 +90,27 @@ public class User {
         this.last_name = last_name;
     }
 
-    public Date getCreated_at() {
+    public String getCreated_at() {
         return created_at;
     }
 
-    public void setCreated_at(Date created_at) {
+    public void setCreated_at(String created_at) {
         this.created_at = created_at;
     }
 
-    public Boolean getActivated_at() {
+    public String getActivated_at() {
         return activated_at;
     }
 
-    public void setActivated_at(Boolean activated_at) {
+    public void setActivated_at(String activated_at) {
         this.activated_at = activated_at;
     }
 
-    public Date getLast_login() {
+    public String getLast_login() {
         return last_login;
     }
 
-    public void setLast_login(Date last_login) {
+    public void setLast_login(String last_login) {
         this.last_login = last_login;
     }
 
@@ -125,5 +129,13 @@ public class User {
                 ", activated_at=" + activated_at +
                 ", last_login=" + last_login +
                 '}';
+    }
+
+    public Boolean getSigned_in() {
+        return signed_in;
+    }
+
+    public void setSigned_in(Boolean signed_in) {
+        this.signed_in = signed_in;
     }
 }
