9a7798d4f50f3fad22fc3ec6cde963b42e8cb373
Thomas Roseman <lindows.dev@gmail.com>
2014-12-09 16:12:34 -0500

    Added blank string to token so we can properly check on login if the user has a blank token

--- app/src/main/java/com/newsappandroid/User.java
+++ app/src/main/java/com/newsappandroid/User.java
@@ -6,7 +6,7 @@ public class User {
 
     private int id;
     private String email, password;
-    private String first_name, last_name, api_token;
+    private String first_name, last_name, api_token = "";
     private String activated_at;
     private Boolean activated;
     private int signed_in = -9;
